d4da63fb6cdc84538ca412e581fa3b42fe7fa3f9
Tyler Wasick <tylerwasick@outlook.com>
2019-01-19 15:23:46 -0600

    Update - 2019.1.19.1523

--- MyFinance/MyFinance/Program.cs
+++ MyFinance/MyFinance/Program.cs
@@ -12,36 +12,8 @@ namespace MyFinance
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            MainMenu();
+            Menu.MainMenu();
             Console.ReadKey();
-        }
-
-        // Print the Main Menu
-        private static void MainMenu()
-        {
-            MenuLine();
-            MenuOptions();
-            MenuLine();
-            Console.WriteLine();
-        }
-
-        private static void MenuLine()
-        {
-            for (int i = 0; i < 119; i++)
-            {
-                Console.Write("*");
-            }
-            //Console.WriteLine();
-        }
-
-        private static void MenuOptions()
-        {
-            Console.WriteLine();
-            for (int i = 0; i < 22; i++)
-            {
-                Console.WriteLine("*");
-            }
-            //Console.WriteLine();
         }
 
     }
